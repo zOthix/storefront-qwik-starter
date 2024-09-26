16a6b7969b0b747926376fc841c6cb9404167a6b
Ghazan <ghazankhan27@hotmail.com>
2024-09-26 17:28:20 +0500

    chore: Added correct api urls

--- src/constants.ts
+++ src/constants.ts
@@ -14,6 +14,6 @@ export const DEFAULT_METADATA_DESCRIPTION =
 export const DEFAULT_METADATA_IMAGE = 'https://qwik-storefront.vendure.io/social-image.png';
 export const DEFAULT_LOCALE = 'en';
 // TODO: replace DEV_API and PROD_API with your dev and prod API urls.
-export const DEV_API = 'https://readonlydemo.vendure.io';
-export const PROD_API = 'https://readonlydemo.vendure.io';
+export const DEV_API = 'http://localhost:3000';
+export const PROD_API = 'http://64.176.164.234:4090';
 export const LOCAL_API = 'http://localhost:3000';
