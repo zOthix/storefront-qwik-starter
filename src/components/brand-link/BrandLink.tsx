38c50a090d0fbca6570774715223801cb1bf0fe4
Ghazan Khan <ghazankhan27@hotmail.com>
2025-04-10 17:45:55 +0500

    feat: Added slider for brands section

--- src/components/brand-link/BrandLink.tsx
+++ src/components/brand-link/BrandLink.tsx
@@ -1,5 +1,4 @@
 import { component$ } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
 import { Image } from 'qwik-image';
 
 type Props = {
@@ -10,14 +9,14 @@ type Props = {
 
 export default component$<Props>(({ name, src, href }) => {
 	return (
-		<Link href={href} class="min-h-[100px] min-w-[50px]">
-			<div class="relative rounded-lg overflow-hidden">
-				<Image layout="fixed" width="300" height="300" src={src} alt={name} />
+		<a href={href}>
+			<div class="h-[100px] w-[100px] flex justify-center relative border rounded-full overflow-hidden">
+				<Image layout="fixed" src={src} />
 				<span class="absolute w-full bottom-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50" />
-				<span class="absolute w-full bottom-2 mt-auto text-center text-xl font-bold text-white">
+				<span class="absolute w-full bottom-2 mt-auto text-center font-bold text-white">
 					{name}
 				</span>
 			</div>
-		</Link>
+		</a>
 	);
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,10 @@
-import { component$, useContext } from '@builder.io/qwik';
+import { component$, useContext, useVisibleTask$ } from '@builder.io/qwik';
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
+import BrandLink from '~/components/brand-link/BrandLink';
 import Carousal from '~/components/carousal/Carousal';
 import CollectionCard from '~/components/collection-card/CollectionCard';
 import LinkCard from '~/components/link-card/LinkCard';
@@ -9,20 +15,65 @@ export default component$(() => {
 
 	const links = ['Link1', 'Link2', 'Link3', 'Link4'];
 
+	useVisibleTask$(() => {
+		new Swiper('.brand-swiper', {
+			direction: 'horizontal',
+			loop: true,
+			pagination: {
+				el: '.swiper-pagination',
+			},
+			navigation: {
+				nextEl: '.swiper-button-next-brand',
+				prevEl: '.swiper-button-prev-brand',
+			},
+			scrollbar: {
+				el: '.swiper-scrollbar-brand',
+			},
+			modules: [Navigation, Pagination],
+			slidesPerView: 6,
+			spaceBetween: 10,
+		});
+	});
+
 	return (
 		<div>
 			<Carousal />
-			<div class="pt-12 xl:max-w-7xl xl:mx-auto xl:px-8">
+			<div class="pt-12 xl:max-w-7xl xl:mx-auto px-2 md:px-6 flex flex-col">
 				<section>
-					<div class="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-2 md:px-6">
+					<div class="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 						{links.map((link) => {
 							return (
 								<LinkCard
+									href="/"
 									src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyW7l-lLoY4nBYEnqA9ydYbUU56CgDtaq-rQ&s"
 									name={link}
 								/>
 							);
 						})}
+					</div>
+				</section>
+				<section>
+					<div class="mt-8">
+						<h2 class="text-2xl font-light tracking-tight text-gray-900">{$localize`Brands`}</h2>
+						<div class="overflow-hidden mt-4">
+							<div class="brand-swiper swiper">
+								<div class="swiper-wrapper">
+									{['Brand1', 'Brand2', 'Brand3', 'Brand4', 'Brand5', 'Brand6', 'Brand7'].map(
+										(brand) => {
+											return (
+												<div class="swiper-slide flex justify-center">
+													<BrandLink
+														name={brand}
+														href="/account/"
+														src="https://images.seeklogo.com/logo-png/31/2/honda-logo-png_seeklogo-310689.png"
+													/>
+												</div>
+											);
+										}
+									)}
+								</div>
+							</div>
+						</div>
 					</div>
 				</section>
 			</div>
